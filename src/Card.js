db07f862632431fbc9e3018647e032d0a0dd433d
Fahad Siddiqui <fahadsiddqui88@gmail.com>
2022-01-27 11:59:16 -0800

    new work comment

--- src/Card.js
+++ src/Card.js
@@ -2,17 +2,33 @@ import React from 'react';
 import weather_image from './weather.gif';
 import './App.css';
 
+
+
+function toDateTime(secs) {
+  var t = new Date(1970, 0, 1); // Epoch
+  t.setSeconds(secs);
+  return t;
+}
+
 function Card(props){
-console.log("card ",props);
-    return(
+  const months = {
+    0: 'Jan',
+    1: 'Feb',
+    2: 'Mar',
+  }
+
+let dt = toDateTime(props.date);
+return(
     <>
       <div className='card'>
           <div className='card-header'>
-            <img src={weather_image} />     
-            <h6 className='date'>Date : {props.day}</h6>       
+            <img className='img' src={weather_image} />     
+            <img className='icon' src={`https://openweathermap.org/img/wn/${props.icon}@2x.png`} />
+            <h6 className='date'>{dt.getDate()}-{months[dt.getMonth()]}-{dt.getFullYear()} </h6>       
+            <h6 className='temp'>{props.day} <span>&#8451;</span></h6>
           </div>
           <div className='card-body'>
-              <h6 className='temp'>Temp : {props.day}</h6>
+              <h6 className='mix'>Min : {props.min} Max : {props.max}</h6> 
               <h6 className='fell'>Feel : {props.fell}</h6> 
               <h6 className='desc'>Description : {props.description}</h6>
           </div>
--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,8 @@ function Weather(props){
 console.log("Prop" , props);
 return(
     <>
-        <Card day={props.data.temp.day} fell={props.data.feels_like.day} description={props.data.weather[0].description}>
+        <Card date={props.data.dt} day={props.data.temp.day} fell={props.data.feels_like.day} 
+        description={props.data.weather[0].description} icon={props.data.weather[0].icon} min={props.data.temp.min} max={props.data.temp.max}>
         </Card>
     </>
 )
